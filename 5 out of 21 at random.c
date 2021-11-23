c5b15ebfadf4bb6f4444cc1a0e478a5f24bc18b1
YanShangxue <81284984+YanShangxue@users.noreply.github.com>
2021-11-23 11:32:42 +0800

    Update 5 out of 21 at random.c

--- 5 out of 21 at random.c
+++ 5 out of 21 at random.c
@@ -4,13 +4,13 @@
 int main(int argc, char const *argv[])
 {
     int a, i, j;
-    srand(time(0));
+    srand(time(0));   //调用time来初始化随机数
     for (i = 0; i < 5;i++)
     {
-        printf("\n");
+        printf("\n");  
         for (j = 0; j<5; j++)
         {
-            a = rand() % 20 + 1;
+            a = rand() % 21 + 1; //用rand函数取随机数后用求余操作使0<=a<=20,再对a加1使1<=a<=21.
             printf("%d\t", a);
         }
     }
