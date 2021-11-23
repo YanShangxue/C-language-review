af7757d8ff25a23fedd6c04f6a8a4ddfa2cba905
YanShangxue <81284984+YanShangxue@users.noreply.github.com>
2021-11-23 14:43:55 +0800

    Update 5 out of 21 at random.c
    增加了排除重复随机数的功能

--- 5 out of 21 at random.c
+++ 5 out of 21 at random.c
@@ -3,15 +3,21 @@
 #include<time.h>
 int main(int argc, char const *argv[])
 {
-    int a, i, j;
+    int a[5], i, j;
     srand(time(0));   //调用time来初始化随机数
     for (i = 0; i < 5;i++)
-    {
-        printf("\n");  
-        for (j = 0; j<5; j++)
+    { 
+        a[i] = rand() % 21 + 1; //用rand函数取随机数后用求余操作使0<=a<=20,再对a加1使1<=a<=21.
+        for ( j = 0; j<i; j++)
         {
-            a = rand() % 21 + 1; //用rand函数取随机数后用求余操作使0<=a<=20,再对a加1使1<=a<=21.
-            printf("%d\t", a);
+          if (a[i]==a[j])
+          {
+              break; 
+          }
         }
+        if(i==j)
+             printf("%d\t", a[i]);   
+        else
+              i--;
     }
 }
